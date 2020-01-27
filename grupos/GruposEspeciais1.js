e5a07f1d0144cb4ae256ff745d45172dd22d2fae
Marcelo <carbono.marcelo@gmail.com>
2020-01-27 00:44:47 -0200

    fim capítulo grupos

--- grupos/GruposEspeciais1.js
+++ grupos/GruposEspeciais1.js
@@ -2,3 +2,11 @@ const text = 'João é calmo, mas no trânsito fica nervoso.'
 
 console.log(text.match(/[\wÀ-ù]+/g))
 
+//positive lookahead
+console.log(text.match(/[\wÀ-ù]+(?=,)/gi))
+console.log(text.match(/[\wÀ-ù]+(?=\.)/gi))
+console.log(text.match(/[\wÀ-ù]+(?=, mas)/gi))
+
+//negative lookahead
+console.log(text.match(/[\wÀ-ù]+\b(?!,)/gi))
+console.log(text.match(/[\wÀ-ù]+[\s|\.](?!,)/gi))
