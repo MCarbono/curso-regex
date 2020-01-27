c4f7ebbae9b3e5881fcc24314263d4ebd11b4616
Marcelo <carbono.marcelo@gmail.com>
2020-01-27 21:26:31 -0200

    fim exercícios

---
+++ receitas/senha.js
@@ -0,0 +1,14 @@
+const text = `
+123456
+cod3r
+QUASE123!
+#OpA1
+#essaSenhaEGrande1234
+
+#Opa1?
+Foi123!
+`
+
+console.log(text.match(/^.{6,20}$/gm))
+console.log(text.match(/^(?=.*[A-Z]).{6,20}$/gm))
+console.log(text.match(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[#@$!%&*_]).{6,20}$/gm))
